fce793d5f3575b2920df488d50e7c2a6b34f8bc6
Your Name <you@example.com>
2022-05-02 09:45:07 +0300

    fixed typos

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
         <section className='about'>
             <h2>About Me</h2>
             <p>
-                Graduated in 2020 from Swansea University earning a bachelor's degree in Computer Science, with a grade of 1st Class Honours. After that I chased my passion of cybersecurity spend a year earning my master's degree from the prestigious Glasgow University in 2022. During this time, I took various opportunities to develop Dapps and Websites while keeping up-to date with the crypto space which am very excited to be a part of and contribute in!
+                Graduated in 2020 from Swansea University earning a bachelor's degree in Computer Science, with a grade of 1st Class Honours. After that I chased my passion of cybersecurity, investing a year to earn my master's degree in Information Security from the prestigious Glasgow University in 2022. During this time, I took various opportunities to develop Dapps and Websites while keeping up-to date with the crypto space which am very excited to be a part of and contribute in!
             </p>
         </section>
     );
